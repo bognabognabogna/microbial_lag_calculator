8dd348c5de4ccbc8e389e69de1fe4b835ea48f39
Maks Nęcki <57145655+jungwirt@users.noreply.github.com>
2023-05-27 15:26:17 +0200

    Update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -5,7 +5,7 @@
 # Learn more about the roles of various files in:
 # * https://r-pkgs.org/tests.html
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
-install.packages("testthat")
+install.packages("testthat", repos = "http://cran.us.r-project.org")
 library(testthat)
 library(miLAG)
 
