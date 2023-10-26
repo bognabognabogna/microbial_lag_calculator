409330a168e84f79aa933df4b6eb1c81c6085f8f
bognabognabogna <bognabognabogna@gmail.com>
2023-10-26 20:36:41 +0200

    new app version
    - allowing OD
    - allowing manual input
    - allowing multiple curves at the same time

--- shiny_app/lag_calulator/app.R
+++ shiny_app/lag_calulator/app.R
@@ -18,8 +18,8 @@ library(shinydisconnect)
 library(remotes)
 # miLAg package can be installed from here:
 #install_github("https://github.com/bognabognabogna/microbial_lag_calulator", dependencies = TRUE, upgrade = "always", force = FALSE)
-library(miLAG)
-
+#library(miLAG)
+source("~/MGG Dropbox/Bogna Smug/Projects/Quiesence/2022_Lags/GitHub/microbial_lag_calulator/R/milags_functions.R")
 
 
 available.methods = list("max growth acceleration",
@@ -51,13 +51,17 @@ ui <- shinyUI(fluidPage(
                          strong("INSTRUCTION"),
                          br(),
                          strong("1.	Upload your dataset."),
-                         div("The accepted file formats are csv and txt. The dataset must contain two columns as specified below:"),
-                         div("The first column: time (preferably in hours)"),
-                         div("The second column: population size (preferably in CFU/mL)"),
+                         div("The accepted file formats are csv and txt. The dataset must contain two required columns as specified below:"),
+                         div("(i) the first column: time (preferably in hours)"),
+                         div("(ii) the second column: population size (preferably in CFU/mL)"),
+                         div("(iii) the third optional column: curve id (an identifier for a growth curve if dataset consists of multiple growth curves)"),
                          div("The population size is recommend to be measured by biomass or CFU values instead of raw absorbance,
                     This is because the correlation between CFU number and absorbance is rarely linear.
-                      However, if you’re unable to provide CFU/biomass values, the calculator will also work for absorbance data.
+                      However, if you’re unable to provide CFU or biomass values, the calculator will also work for absorbance data (assuming OD is directly proportional to the CFU).
                       After uploading your dataset, specify the column and decimal separators."),
+                         div("If your dataset consists of multiple growth curves, they should be provided in long format i.e. one curve under another and identified by the curve_id provided in the third column.
+                             If multiple growth curves are technical replicates, we advise to fit the lag to the averaged growth curve, 
+                             otherwise lags can be fitted separately to each growth curve."),
                          strong("2. Use some data pre-processing if needed"),
                          br(),
                          strong("3.	Chose lag duration calculation method"),
@@ -110,14 +114,21 @@ ui <- shinyUI(fluidPage(
                                              verbatimTextOutput("message"),
                                          ),
                                          br(),
+                                         conditionalPanel("input.manual == 'No'",
                                          fileInput("growth.curve.file",
-                                                   "Choose CSV or TXT File (first column = time, second column = CFU/mL)",
+                                                   "Choose CSV or TXT File (first column = time, second column = biomass)",
                                                    multiple = FALSE,
                                                    accept = c("text/csv","text/comma-separated-values,text/plain", ".csv"),
                                                    placeholder = "No file selected, using example data:",
-                                                   width = '80%'),
+                                                   width = '80%')),
                          )),
-                         fluidRow(
+                         conditionalPanel("input.manual == 'Yes'",
+                          fluidRow(
+                           column(12, align="center",
+                                  textAreaInput("pasted.data", "Paste your tabular data from a spreadsheet:"),
+                         ))),
+                         conditionalPanel("input.manual == 'No'",
+                          fluidRow(
                            column(6, align="center",
                                   selectInput("column.separator",
                                               label = "column separator",
@@ -128,7 +139,26 @@ ui <- shinyUI(fluidPage(
                                               label = "decimal separator",
                                               choices = c(",", "."),
                                               selected = ","))),
+                          ),
+                         fluidRow(
+                           column(12, align="center",
+                                  selectInput("manual",
+                                              label = "Would you prefer to input data manualy (paste from Excel)?",
+                                              choices = c("Yes", "No"),
+                                              selected = "No"))),
                          br(),
+                         fluidRow(
+                           column(12, align = "center",
+                         selectInput("biomass_unit",
+                                     label = "Biomass unit",
+                                     choices = c("CFU/ml", "OD"),
+                                     selected = ","))),
+                         fluidRow(
+                           column(12, align = "center",
+                                  selectInput("separate.fit",
+                                              label = "Do you want to fit lag to every curve separately or to the mean?",
+                                              choices = c("Separete lags", "Fit lag to the mean"),
+                                              selected = "Separate lags"))),
                          h5("Provided growth curve data"),
                          DT::dataTableOutput("growth.curve.data.table", width = '80%'),
                          br(),
@@ -153,16 +183,13 @@ ui <- shinyUI(fluidPage(
                                        choices = c("no", "yes"),
                                        selected = "no"),
                            br(),
-                           #conditionalPanel(condition = "input.smooth_data_flag == 'yes'",
-                           #                 selectInput("smooth_method",
-                           #                              label = "Choose the Tukey's smoother",
-                           #                              choices = c('3RS3R', '3RSS', '3R'),
-                           #                              selected = '3RS3R')),
+                             selected = '3RS3R')),
                          ),
                          mainPanel(
+                           verbatimTextOutput("not_enough_many_points_message"),
                            br(),
                            h5("Growth curve data after pre-processing"),
-                           plotOutput("growth.curve.data.processed.plot"),
+                           uiOutput("growth.curve.data.processed.plot"),
                            br(),
                          )),
                 tabPanel("Lag calculation",
@@ -248,10 +275,12 @@ ui <- shinyUI(fluidPage(
                                            br(),
                                            h5("Lag fitting and growth curve data plot"),
                                            tableOutput("lag.value"),
+                                           tableOutput("lag.stats"),
                                            br(),
                                            h5("Note that you may be able improve the efficiency of the methods by varying the parameters or further pre-processing the data.
               For example, if parameter fitting method is chosen it may be helpful to cut out the stationary phase data (within data pre-processing tab)."),
-                                           plotOutput("growth.curve.plot"),
+                                           uiOutput("growth.curve.plot"),
+                                           #plotOutput("growth.curve.plot"),
                                          ))),width = 8)
     )
 
@@ -264,7 +293,7 @@ ui <- shinyUI(fluidPage(
 
 
 
-server <- shinyServer(function(input, output) {
+server <- shinyServer(function(input, output, session) {
   output$Decission_Tree <- renderImage({
 
     list(src = "www/Decission_Tree.png",
@@ -303,11 +332,15 @@ server <- shinyServer(function(input, output) {
              Note: They must be numeric and non-negative"
     }
   })
+  
+  
+
+################################################## Tab 2: read data and display it ################################################## 
 
   growth.curve.data = reactive({
     message = ""
     if (is.null(input$growth.curve.file)) {
-      data.path = "R/example_data.csv"
+      data.path = "R/simulated_data_multiple_curves.csv" #"R/example_data.csv"
     } else {
       data.path =  input$growth.curve.file$datapath
     }
@@ -321,67 +354,166 @@ server <- shinyServer(function(input, output) {
 
     if (!(data.format == "csv" | data.format == "txt")) {
       message = "data format not supported"
-    } else {
+    }  else {
       out = tryCatch(
         {
-          # in case there is a header ignore all non-numeric values
-          # make sure the decimal is always "." so that we can convert it to string and back to numeric
-          data =  read.table(data.path,
-                             header = FALSE,
-                             dec = input$dec,
-                             sep = column.separator,
-                             stringsAsFactors = FALSE)
-          data = mutate_all(data, function(x) as.numeric(sub(input$dec, ".", as.character(x), fixed = TRUE)))
-          names(data) = c("time", "biomass")
-          data = data %>% select(time, biomass) %>%
-            filter(!is.na(time) & !is.na(biomass))
+          if (input$manual == "Yes") {
+            data = data.table::fread(paste(input$pasted.data, collapse = "\n")) %>% as.data.frame()
+          } else {
+            # in case there is a header ignore all non-numeric values
+            # make sure the decimal is always "." so that we can convert it to string and back to numeric
+            data =  read.table(data.path,
+                               header = FALSE,
+                               dec = input$dec,
+                               sep = column.separator,
+                               stringsAsFactors = FALSE)
+          }
+          
+          if (ncol(data) < 2) {
+            error("At least two columns are needed (time and biomass)")
+          } else if (ncol(data) == 2) {
+            data = data %>% mutate(curve_id = "provided_growth_curve")
+          }
+          names(data) = c("time", "biomass", "curve_id")
+          data = mutate_at(data, c("time", "biomass"), function(x) as.numeric(sub(input$dec, ".", as.character(x), fixed = TRUE)))
+          
+          data = data %>% 
+            filter(!is.na(time) & !is.na(biomass) & !is.na(biomass)) %>%
+            select(time, biomass, curve_id)
+          
           return(list(data=data, message = message))
         },
         error=function(cond) {
           message = "Can't read in the data. Are the separators specified correctly?"
           # Choose a return value in case of error
-          return(list(data=data.frame(time = numeric(0), biomass = numeric(0)),
+          return(list(data=data.frame(time = numeric(0), biomass = numeric(0), curve_id = character(0)),
                       message = message))
         }
       )
-
+      
       return(out)
     }
   })
-
-
+  
 
   output$growth.curve.data.table <- DT::renderDataTable(growth.curve.data()$data,
                                                         rownames= FALSE,
                                                         options = list(searching = FALSE))
+  
 
-  growth.curve.data.cut = reactive({
-    if (input$cut_data_flag == 'yes') {
-      data = cut_the_data(growth.curve.data()$data, input$cut_max_time)
+  
+  
+  ################################################## Tab 3: data plot and preprocessing ################################################## 
+  log10_transform_param = reactive({
+    if (input$biomass_unit == "CFU/ml") {
+      return(TRUE)
+    } else if (input$biomass_unit == "OD") {
+      return(FALSE)
     } else {
+      error("Incorrect biomass_unit")
+    }
+    })
+    
+    y.label = reactive({
+      if (log10_transform_param()) {
+        return(paste0("log10(", input$biomass_unit, ")"))
+      } else {
+        return("OD")
+      } 
+    })
+    
+    
+  
+    # for CFU/mL work on log scale
+    growth.curve.data.sd = reactive({
       data = growth.curve.data()$data
-    }
-    return(data)
-  })
+      if (input$separate.fit == "Fit lag to the mean") {
+        if (log10_transform_param()) { 
+          data = data %>%
+            mutate(biomass = log10(biomass))
+        }
+        sd.data = data %>% 
+          group_by(time) %>%
+          summarise(
+            SD = sd(biomass, na.rm = TRUE)) %>%
+          ungroup()
+      } else {
+        sd.data = data %>% distinct(time) %>% mutate(SD = NA)
+      }
+      return(sd.data)
+    })
+    
+    
+    data.for.lag.calculation = reactive({
+      if (input$separate.fit == "Fit lag to the mean") {
+        data = growth.curve.data()$data %>%
+          group_by(time) %>%
+          summarise(biomass = mean(biomass, na.rm = TRUE)) %>%
+          ungroup() %>%
+          mutate(curve_id = "Mean growth curve")
+      } else {
+        data = growth.curve.data()$data
+      }
+      return(data)
+    })
+    
+    num.curves = reactive({data.for.lag.calculation() %>% distinct(curve_id) %>% nrow()})
+    
+    
+    
+    growth.curve.data.cut = reactive({
+      data = data.for.lag.calculation()
+      if (input$cut_data_flag == 'yes') {
+        data = cut_the_data(data, input$cut_max_time)
+      } 
+      return(data)
+    })
+    
+    
+    growth.curve.data.processed = reactive({
+      if (input$smooth_data_flag == 'yes') {
+        data.smooth = smooth_data(growth.curve.data.cut(), '3RS3R') #input$smooth_method)
+      } else {
+        data.smooth = growth.curve.data.cut()
+      }
+      return(data.smooth)
+    })
+    
+    output$not_enough_many_points_message <- renderText({
+      if (nrow(growth.curve.data.processed()) < 4) {
+        return("Not enough many time points")
+      } else {
+        return("")
+      }
 
-
-  growth.curve.data.processed = reactive({
-    if (input$smooth_data_flag == 'yes') {
-      data.smooth = smooth_data(growth.curve.data.cut(), '3RS3R') #input$smooth_method)
-    } else {
-      data.smooth = growth.curve.data.cut()
-    }
-    #return(list(data = data,
-    #            message = growth.curve.data()$message))
-    return(data.smooth)
-  })
-
+    })
+    
+    output$growth.curve.data.processed.plot.raw = renderPlot({
+      data.to.plot = growth.curve.data.processed() %>%
+        left_join(growth.curve.data.sd())
+      if (nrow(data.to.plot) > 0) {
+        fig = plot_data(data_new = data.to.plot, log10_transform = log10_transform_param()) + 
+          my_theme +
+          scale_y_continuous() + 
+          facet_grid(curve_id~ .) +
+          ylab(y.label()) +
+          geom_errorbar(aes(ymin = biomass - SD, ymax = biomass + SD))
+      } else
+        fig = ggplot() + theme_void() + 
+          ylab(y.label())
+      return(fig)
+    })
+    
+    output$growth.curve.data.processed.plot =   renderUI({
+      plotOutput("growth.curve.data.processed.plot.raw", height = 250*num.curves())
+    })
+    
+  ################################################## Tab 4: Lag calculation and data plot ################################################# 
   growth.curve.data.with.lag =  reactive({
     if (nrow(growth.curve.data.processed()) > 0) {
       data.with.lag = calc_lag(data = growth.curve.data.processed(),
                                     method = input$method,
-                                    pars = model.params()) %>%
-        mutate(lag.calculation.method = input$method)
+                                    pars = model.params())
     } else {
       data.with.lag = growth.curve.data.processed()
     }
@@ -395,51 +527,63 @@ server <- shinyServer(function(input, output) {
 
   lag.value = reactive({
     if (nrow(growth.curve.data.with.lag()) > 0) {
-      lag.info = round(unique(growth.curve.data.with.lag()$lag),3)
-      t = data.frame(method = input$method, lag = lag.info)
+      lag.data.table = growth.curve.data.with.lag() %>%
+        distinct(lag, curve_id) %>%
+        rowwise() %>%
+        mutate(lag.info = round(lag,3),
+               method = input$method) %>%
+        ungroup() %>%
+        select(curve_id, lag = lag.info, method)
+
     } else {
-      t = data.frame(method = input$method, lag = NA)
-      return(t)
+      lag.data.table = data.frame(method = input$method, lag = NA)
+      return(lag.data.table)
     }
   })
 
+  lag.stats = reactive({
+    data.frame(
+      lag.mean = lag.value()$lag %>% mean(na.rm = TRUE),
+      lag.sd = lag.value()$lag %>% sd(na.rm = TRUE),
+      lag.min = lag.value()$lag %>% min(na.rm = TRUE),
+      lag.max = lag.value()$lag %>% max(na.rm = TRUE))
+  })
+    
   output$lag.value = renderTable(lag.value(),
+                                 rownames= FALSE)
+  output$lag.stats = renderTable(lag.stats(),
                                  rownames= FALSE)
 
 
 
-  output$growth.curve.plot = renderPlot({
+  
+  output$plot = renderPlot({
     if (nrow(growth.curve.data.with.lag()) > 0) {
-      fig = plot_lag_fit(growth.curve.data.with.lag()) + my_theme
-    } else
-      fig = ggplot() + theme_void()
-    return(fig)
-  })
-
-
-  output$growth.curve.data.processed.plot = renderPlot({
-    if (nrow(growth.curve.data.processed()) > 0) {
-      fig = plot_data(growth.curve.data.processed()) + 
+      data.to.plot = growth.curve.data.with.lag() %>%
+        left_join(growth.curve.data.sd())
+      # seems like the NA fail here, try without the unnecessary tangent lines etc
+      #write.csv(data.to.plot, "/Users/bognasmug/MGG Dropbox/Bogna Smug/Projects/Quiesence/2022_Lags/GitHub/microbial_lag_calulator/shiny_app/lag_calulator/R/test_data/test.csv", row.names = FALSE)
+      fig = plot_lag_fit(data.to.plot, log10_transform = log10_transform_param()) + 
         my_theme +
-        scale_y_continuous()
-    } else
-      fig = ggplot() + theme_void()
+        ylab(y.label())  +
+        geom_errorbar(aes(ymin = biomass - SD, ymax = biomass + SD))
+    } else {
+      fig = ggplot() + theme_void() + ylab(y.label())
+    }
     return(fig)
   })
 
-
+  output$growth.curve.plot =   renderUI({
+    plotOutput("plot", height = 250*num.curves())
+  })
+  
   observeEvent(input$disconnect, {
     session$close()
   })
+  session$onSessionEnded(function() {
+    stopApp()
+  })
 
-  #observeEvent(input$login, {
-  #  showModal(modalDialog(
-  #    title = "You have logged in.",
-  #    paste0("aaa"),
-  #    easyClose = TRUE,
-  #    footer = NULL
-  #  ))
-  #})
 })
 
 # Run the application
