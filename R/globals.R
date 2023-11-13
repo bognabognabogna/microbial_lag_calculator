db82092fed8f2415d9d728998159677fb90ada7c
Maks Nęcki <57145655+jungwirt@users.noreply.github.com>
2023-11-13 03:48:13 +0100

    Update globals.R
    changed logdata and biomass

--- R/globals.R
+++ R/globals.R
@@ -2,7 +2,7 @@ globalVariables(
   c(
     "LOG10N", "anova", "baranyi", "biomass", "biomass_smooth", "central_scheme_db",
     "central_scheme_dt", "coef", "curve_id", "lag_calculation_method", "lag_info",
-    "line_intercept", "line_slope", "lm", "log_biomass", "logdata max_biomass",
+    "line_intercept", "line_slope", "lm", "log_biomass", "logdata", "max_biomass",
     "max_second_deriv_b", "max_threshold", "median", "min_second_deriv_b",
     "min_threshold", "n0", "nls", "nls.control", "nlsLM", "predict", "predicted",
     "predicted_data", "second_deriv_b", "second_deriv_b_scaled", "smooth",
