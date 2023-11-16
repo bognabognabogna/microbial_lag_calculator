22be1b1a996ec88b7d64708ff04533dd7e68f763
Maks Nęcki <57145655+jungwirt@users.noreply.github.com>
2023-11-16 15:39:04 +0100

    Update testthat.R
    excluded installation

--- tests/testthat.R
+++ tests/testthat.R
@@ -5,10 +5,8 @@
 # Learn more about the roles of various files in:
 # * https://r-pkgs.org/tests.html
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
-install.packages("testthat", repos = "http://cran.us.r-project.org")
-install.packages("ggplot2", repos = "http://cran.us.r-project.org")
-library(ggplot2)
-library(testthat)
-library(miLAG)
 
+library(miLAG)
+library(dplyr)
+library(testthat)
 test_check("miLAG")
